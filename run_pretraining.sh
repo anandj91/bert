890b375819e0acc4aca638bd22e4300df7275474
Anand J <anandj@cs.ubc.ca>
2019-07-27 00:57:01 -0700

    BERT training changes

---
+++ run_pretraining.sh
@@ -0,0 +1,14 @@
+BERT_BASE_DIR=/home/anandj/data/code/bert/pretrain
+rm -rf $BERT_BASE_DIR/pretraining_output
+python run_pretraining.py \
+  --input_file=$BERT_BASE_DIR/tf_examples.tfrecord \
+  --output_dir=$BERT_BASE_DIR/pretraining_output \
+  --do_train=True \
+  --do_eval=False \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --train_batch_size=$1 \
+  --max_seq_length=128 \
+  --max_predictions_per_seq=20 \
+  --num_train_steps=1000 \
+  --num_warmup_steps=10 \
+  --learning_rate=2e-5
